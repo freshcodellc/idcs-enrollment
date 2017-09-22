b3f66a487e6dda62accc74aac5bc38ab00014349
Lance Sorenson <lancesorenson@gmail.com>
2017-09-22 14:23:09 -0600

    Moved IDCS API calls to separate class

---
+++ app/Api/IdcsApi.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace App\Api;
+
+use App\CreditUrl;
+use \Illuminate\Contracts\Auth\Authenticatable;
+
+class IdcsApi
+{
+    private $user = null;
+
+    /**
+     * @var bool Flag on whether to save an existing URL to the CreditUrl model
+     */
+    private $saveExistingUrl = false;
+
+    private $endpoints = [
+        "enroll" => "http://xml.idcreditservices.com/IDSWebServicesNG/IDSEnrollment.asmx?WSDL",
+        "getCreditUrl" => "https://xml.idcreditservices.com/SIDUpdateServices/MemberUpdate.asmx?WSDL"
+    ];
+
+    public function __construct(Authenticatable $user)
+    {
+        $this->user = $user;
+    }
+
+    public function enroll()
+    {
+        $client = new \SoapClient($this->endpoints['enroll'], array(
+            'login' => env('IDCS_USERNAME'),
+            'password' => env('IDCS_PASSWORD'),
+            'trace' => true,
+            'cache_wsdl' => WSDL_CACHE_NONE,
+            //'soap_version' => SOAP_1_2,
+            //'use' => SOAP_LITERAL
+            //'features' => SOAP_SINGLE_ELEMENT_ARRAYS
+        ));
+
+        $parameters = $this->getEnrollParameters();
+
+        try {
+            $result_xml = $client->IDSEnrollmentXML($parameters);
+
+            //echo "<h3>RESULT:</h3><pre>";
+            //print_r($result_xml);
+            //echo "</pre>";
+
+
+
+        } catch (\Exception $e) {
+            if (env("APP_DEBUG")) {
+                echo "<h3>EXCEPTION:</h3>";
+                echo $e->getMessage(). " on " . $e->getFile(). ":".$e->getLine();
+
+                echo "<h3>REQUEST:</h3><pre>";
+                echo $client->__getLastRequestHeaders();
+                echo htmlentities($client->__getLastRequest());
+                echo "</pre>";
+            } else {
+                throw $e;
+            }
+        }
+
+        // Save Credit URL for user
+        $result = new \SimpleXMLElement($result_xml->IDSEnrollmentXMLResult->any);
+        //echo "<h3>RESULT out of XML:</h3><pre>";
+        //print_r($result);
+        //echo "</pre>";
+
+        $credit_url = new CreditUrl;
+
+        if ($result->Status == "SUCCESS") {
+            if (isset($result->CreditReportInfo->CreditReportUrl)) {
+                $credit_url->url = $result->CreditReportInfo->CreditReportUrl;
+                $credit_url->sale_id = $result->SaleId;
+                $credit_url->save();
+            }
+        } else {
+            // fail response
+            if ($result->ErrorCode == "PART_608_F") {
+                // duplicate memberId, so lets get the existing credit url
+                // this is broke ---> $this->getExistingCreditUrl();
+            }
+        }
+
+        return $result;
+    }
+
+    public function getExistingCreditUrl()
+    {
+        $client = new \SoapClient($this->endpoints['getCreditUrl'], array(
+            'login' => env('IDCS_USERNAME'),
+            'password' => env('IDCS_PASSWORD'),
+            'trace' => true,
+            'cache_wsdl' => WSDL_CACHE_NONE,
+            //'soap_version' => SOAP_1_2,
+            //'use' => SOAP_LITERAL
+            //'features' => SOAP_SINGLE_ELEMENT_ARRAYS
+        ));
+
+        //dd($client->__getTypes());
+        //dd($client->__getFunctions());
+
+        $parameters = [
+            "partnerAccount" => env("IDCS_USERNAME"),
+            "partnerPassword" => env("IDCS_PASSWORD"),
+            "MemberId" => $this->user->uuid
+        ];
+
+        $xml = new \SimpleXMLElement("<GetCreditURL></GetCreditURL>");
+
+        $this->array_to_xml($parameters, $xml);
+        $raw_xml = $xml->asXML();
+        $raw_xml = trim(str_replace('<?xml version="1.0"?>', '', $raw_xml));
+
+        $parameters = new \SoapVar($raw_xml, XSD_ANYXML);
+
+        //$parameters = json_decode(json_encode($parameters));
+        //dd($parameters);
+
+        try {
+            // THIS IS BROKE TOO
+            $result_xml = $client->GetCreditURL($parameters);
+
+            echo "<h3>RESULT:</h3><pre>";
+            print_r($result_xml);
+            echo "</pre>";
+
+            if ($this->saveExistingUrl) {
+                // TODO: Save existing URL to user
+            }
+
+            return $result_xml;
+        } catch (\Exception $e) {
+            if (env("APP_DEBUG")) {
+                echo "<h3>EXCEPTION:</h3>";
+                echo $e->getMessage(). " on " . $e->getFile(). ":".$e->getLine();
+
+                echo "<h3>REQUEST:</h3><pre>";
+                echo $client->__getLastRequestHeaders();
+                echo htmlentities($client->__getLastRequest());
+                echo "</pre>";
+            } else {
+                throw $e;
+            }
+        }
+    }
+
+    /**
+     * Get parameters to enroll with IDCS
+     *
+     * @return array
+     */
+    private function getEnrollParameters()
+    {
+        // array we need to convert to XML
+        $parameters = [
+            "RequestSource" => "PartnerCode",
+            "Product" => [
+                "PackageId" => env('IDCS_PACKAGE'),
+                "ProductUser" => [
+                    "Memberid" => $this->user->uuid,
+                    "EmailAddress" => "test321@test.com",
+                    "Password" => "123Password321",
+                    "Address" => [
+                        "Address1" => $this->user->address,
+                        "Address2" => "",
+                        "City" => $this->user->city,
+                        "State" => $this->user->state,
+                        "ZipCode" => $this->user->zip,
+                    ],
+                    "Phone" => [
+                        "PhoneNumber" => $this->user->phone,
+                        "PhoneType" => "Home"
+                    ],
+                    "Person" => [
+                        "FirstName" => $this->user->first_name,
+                        "LastName" => $this->user->last_name,
+                        "MiddleName" => ""
+                    ]
+                ]
+            ],
+            "Partner" => [
+                "partnerAccount" => env('IDCS_USERNAME'),
+                "partnerCode" => env('IDCS_USERNAME'),
+                "partnerPassword" => env('IDCS_PASSWORD'),
+                "Branding" => env('IDCS_USERNAME')
+            ]
+        ];
+
+
+        $xml = new \SimpleXMLElement("<Request></Request>");
+
+        $this->array_to_xml($parameters, $xml);
+        $raw_xml = $xml->asXML();
+        $raw_xml = trim(str_replace('<?xml version="1.0"?>', '', $raw_xml));
+
+        $parameters = [
+            'xmlRequest' => [
+                'any' => new \SoapVar($raw_xml, XSD_ANYXML)
+            ]
+        ];
+
+        return $parameters;
+    }
+
+    /**
+     * Convert an array to XML
+     */
+    private function array_to_xml($array, &$xml_user_info)
+    {
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                if (!is_numeric($key)){
+                    $subnode = $xml_user_info->addChild("$key");
+                    $this->array_to_xml($value, $subnode);
+                } else {
+                    $subnode = $xml_user_info->addChild("item$key");
+                    $this->array_to_xml($value, $subnode);
+                }
+            } else {
+                $xml_user_info->addChild("$key",htmlspecialchars("$value"));
+            }
+        }
+    }
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Api\IdcsApi;
 use App\CreditUrl;
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -27,123 +29,14 @@ class HomeController extends Controller
     {
         $credit_urls = CreditUrl::where('user_id', Auth::user()->id)->get();
 
-        if (count($credit_urls) == 0) {
-            $this->enroll();
-        }
+        //if (count($credit_urls) == 0) {
+        //    $idcs_api = new IdcsApi(Auth::user());
+        //    $idcs_api->enroll();
+        //}
 
         return view('home', [
             'credit_urls' => $credit_urls
         ]);
     }
 
-    public function enroll()
-    {
-        $client = new \SoapClient(env('IDCS_ENROLL_ENDPOINT'), array(
-            'login' => env('IDCS_USERNAME'),
-            'password' => env('IDCS_PASSWORD'),
-            'trace' => true,
-            'cache_wsdl' => WSDL_CACHE_NONE,
-            //'soap_version' => SOAP_1_2,
-            //'use' => SOAP_LITERAL
-            //'features' => SOAP_SINGLE_ELEMENT_ARRAYS
-        ));
-
-        $parameters = $this->getEnrollParameters();
-
-        try {
-            $result = $client->IDSEnrollmentXML($parameters);
-            dd($result);
-            return $result;
-        } catch (\Exception $e) {
-            if (env("APP_DEBUG")) {
-                echo "<h3>EXCEPTION:</h3>";
-                echo $e->getMessage(). " on " . $e->getFile(). ":".$e->getLine();
-
-                echo "<h3>REQUEST:</h3><pre>";
-                echo $client->__getLastRequestHeaders();
-                echo htmlentities($client->__getLastRequest());
-                echo "</pre>";
-            } else {
-                throw $e;
-            }
-        }
-    }
-
-    /**
-     * Get parameters to enroll with IDCS
-     *
-     * @return array
-     */
-    private function getEnrollParameters()
-    {
-        // array we need to convert to XML
-        $parameters = [
-            "RequestSource" => "PartnerCode",
-            "Product" => [
-                "PackageId" => env('IDCS_PACKAGE'),
-                "ProductUser" => [
-                    "Memberid" => Auth::user()->uuid,
-                    "EmailAddress" => Auth::user()->email,
-                    "Password" => "123Password321",
-                    "Address" => [
-                        "Address1" => Auth::user()->address,
-                        "Address2" => "",
-                        "City" => Auth::user()->city,
-                        "State" => Auth::user()->state,
-                        "ZipCode" => Auth::user()->zip,
-                    ],
-                    "Phone" => [
-                        "PhoneNumber" => Auth::user()->phone,
-                        "PhoneType" => "Home"
-                    ],
-                    "Person" => [
-                        "FirstName" => Auth::user()->first_name,
-                        "LastName" => Auth::user()->last_name,
-                        "MiddleName" => ""
-                    ]
-                ]
-            ],
-            "Partner" => [
-                "partnerAccount" => env('IDCS_USERNAME'),
-                "partnerCode" => env('IDCS_USERNAME'),
-                "partnerPassword" => env('IDCS_PASSWORD'),
-                "Branding" => env('IDCS_USERNAME')
-            ]
-        ];
-
-
-        $xml = new \SimpleXMLElement("<Request></Request>");
-
-        $this->array_to_xml($parameters, $xml);
-        $raw_xml = $xml->asXML();
-        $raw_xml = trim(str_replace('<?xml version="1.0"?>', '', $raw_xml));
-
-        $parameters = [
-            'xmlRequest' => [
-                'any' => new \SoapVar($raw_xml, XSD_ANYXML)
-            ]
-        ];
-
-        return $parameters;
-    }
-
-    /**
-     * Convert an array to XML
-     */
-    private function array_to_xml($array, &$xml_user_info)
-    {
-        foreach ($array as $key => $value) {
-            if (is_array($value)) {
-                if (!is_numeric($key)){
-                    $subnode = $xml_user_info->addChild("$key");
-                    $this->array_to_xml($value, $subnode);
-                } else {
-                    $subnode = $xml_user_info->addChild("item$key");
-                    $this->array_to_xml($value, $subnode);
-                }
-            } else {
-                $xml_user_info->addChild("$key",htmlspecialchars("$value"));
-            }
-        }
-    }
 }
