7aaa16b50298e14ab7b5331fb60e170fa56dbd57
Neum <neum@pageworthy.com>
2017-10-05 12:55:58 -0600

    change app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Credit Project'),
+    'name' => env('APP_NAME', 'See Your Score'),
 
     /*
     |--------------------------------------------------------------------------
