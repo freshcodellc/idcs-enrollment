904712e713ac20a8f8ddb1123da5901966e436d4
Neum <neum@pageworthy.com>
2017-10-05 08:44:12 -0600

    chill the sidebar icon boldness

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-4 thesidebar">
     <div class="row">
         <div class="col-md-2">
-            <i class="fa fa-clock fa-3x"></i>
+            <i class="far fa-clock fa-3x"></i>
         </div>
         <div class="col-md-10">
             <h4>Around the clock credit monitoring service.</h4>
@@ -9,7 +9,7 @@
     </div>
     <div class="row spaced-row">
         <div class="col-md-2">
-            <i class="fa fa-dollar-sign fa-3x"></i>
+            <i class="far fa-dollar-sign fa-3x"></i>
         </div>
         <div class="col-md-10">
             <h4>$1M in credit restoration protection.</h4>
