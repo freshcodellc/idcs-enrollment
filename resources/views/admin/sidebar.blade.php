f866e37dc6a6b722568cfca618a23317099e26f4
Lance Sorenson <lancesorenson@gmail.com>
2017-10-08 17:57:31 -0600

    Minimized navigation sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,24 +1,7 @@
-<div class="col-md-3">
-
-    @foreach($laravelAdminMenus->menus as $section)
-        @if($section->items)
-            <div class="panel panel-default panel-flush">
-                <div class="panel-heading">
-                    {{ $section->section }}
-                </div>
-
-                <div class="panel-body">
-                    <ul class="nav" role="tablist">
-                        @foreach($section->items as $menu)
-                            <li role="presentation">
-                                <a href="{{ url($menu->url) }}">
-                                    {{ $menu->title }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-        @endif
-    @endforeach
+<div class="list-group">
+    <a class="list-group-item" href="{{ url('admin') }}"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp; Dashboard</a>
+    <a class="list-group-item" href="{{ url('admin/users') }}"><i class="fa fa-user fa-fw" aria-hidden="true"></i>&nbsp; Users</a>
+    <a class="list-group-item" href="{{ url('admin/charges') }}"><i class="fa fa-pencil fa-fw" aria-hidden="true"></i>&nbsp; Charges</a>
+    <a class="list-group-item" href="{{ url('admin/subscriptions') }}"><i class="fa fa-cog fa-fw" aria-hidden="true"></i>&nbsp; Subscriptions</a>
 </div>
+
