6974205a6f65110a7c8ded20d4408b7865a28642
Neum <neum@pageworthy.com>
2017-10-11 16:06:57 -0600

    updated welcome page content and styles

--- resources/views/layouts/n_app.blade.php
+++ resources/views/layouts/n_app.blade.php
@@ -86,307 +86,34 @@
     </header>
     <!-- End Header -->
 
-    <!-- Signup -->
-    <section class="clearfix">
-      <div class="row no-gutters align-items-center">
-        <div class="col-lg-6">
-            <!-- <div class="js-slide g-bg-img-hero g-min-height-100vh--lg" style="background-image: url(../../assets/img-temp/900x900/img3.jpg);" data-calc-target="#js-header"></div> -->
-            <img class="img-fluid g-bg-img-hero g-min-height-100vh--lg" src="{{ asset('i/b_square.jpg') }}" alt="girl looking at computer">
-        </div>
+    <div class="container">
+            <div class="row">
 
-        <div class="col-lg-6">
-          <div class="g-pa-40 g-mx-70--xl">
-            <!-- Form -->
-            <h1>See Your Credit Scores Now</h1>
-            <h4>Get your full credit report from all three credit reporting agencies in minutes. Begin the 3 simple steps by filling out the form below.</h4>
-            <p><em>Checking your credit will not harm your score.</em></p>
-            @if (empty(Auth::user()->id))
-
-            <form class="form-horizontal startform" method="POST">
-                {{ csrf_field() }}
-
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
-                            <label for="first_name" class="col-md-4 control-label">First Name</label>
-                            <div class="col-md-8 startformfield">
-                                <input id="first_name" type="text" class="form-control" name="first_name" value="{{ old('first_name') }}" placeholder="First Name" required>
-
-                                @if ($errors->has('first_name'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('first_name') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
-                            <label for="last_name" class="col-md-4 control-label">Last Name</label>
-
-                            <div class="col-md-8 startformfield">
-                                <input id="last_name" type="text" class="form-control" name="last_name" value="{{ old('last_name') }}" placeholder="Last Name" required>
-
-                                @if ($errors->has('last_name'))
-                                <span class="help-block">
-                                    <strong>{{ $errors->first('last_name') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Email</label>
-
-                            <div class="col-md-8 startformfield">
-                                <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email" required>
-
-                                @if ($errors->has('email'))
-                                <span class="help-block">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <button type="submit" class="btn btn-warning btn-lg text-center startbtn">
-                    GET STARTED NOW
-                </button>
-            </form>
-            @else
-            <a class="btn btn-warning btn-lg text-center startbtn" href="{{ route('home') }}">
-                Go to My Account
-            </a>
-            @endif
-            <!-- End Form -->
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- End Signup -->
-
-
-
-
-
-
-
-    <!-- Promo Block -->
-    <!-- <section class="dzsparallaxer auto-init height-is-based-on-content use-loading mode-scroll loaded dzsprx-readyall" data-options='{direction: "reverse", settings_mode_oneelement_max_offset: "150"}'>
-      <div class="divimage dzsparallaxer--target w-100 g-bg-pos-bottom-center" style="height: 120%; background-image: url({{ asset('i/banner3darker.jpg') }});"></div>
-      <div class="container g-py-100">
-        <div class="row">
-          <div class="col-md-6">
-            <h3 class="banner-text g-color-white g-font-weight-300 g-font-size-40 g-line-height-1_2 mb-4">See Your Credit Score<br>&amp; Full Report
-              </h3>
-            <span class="banner-text d-block g-color-white-opacity-0_8 g-font-size-16 mb-5">From All 3 Bureaus</span>
-          </div>
-          <div class="col-md-6 g-color-white">
-            <h3>See Your Scores Now</h3>
-                <p>Get your full credit report from all three credit reporting agencies in minutes.</p>
-                <p><em>Checking your credit will not harm your score.</em></p>
-                @if (empty(Auth::user()->id))
-
-                <form class="form-horizontal startform" method="POST">
-                    {{ csrf_field() }}
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
-                                <label for="first_name" class="col-md-4 control-label">First Name</label>
-                                <div class="col-md-8 startformfield">
-                                    <input id="first_name" type="text" class="form-control" name="first_name" value="{{ old('first_name') }}" placeholder="First Name" required>
-
-                                    @if ($errors->has('first_name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('first_name') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
-                                <label for="last_name" class="col-md-4 control-label">Last Name</label>
-
-                                <div class="col-md-8 startformfield">
-                                    <input id="last_name" type="text" class="form-control" name="last_name" value="{{ old('last_name') }}" placeholder="Last Name" required>
-
-                                    @if ($errors->has('last_name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('last_name') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">Email</label>
-
-                                <div class="col-md-8 startformfield">
-                                    <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email" required>
-
-                                    @if ($errors->has('email'))
-                                    <span class="help-block">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <button type="submit" class="btn btn-primary btn-lg btn-warning text-center startbtn">
-                        GET STARTED NOW
+                @if (isset($success) && !empty($success))
+                <div class="col-md-8 col-md-offset-2 alert alert-success" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
                     </button>
-                </form>
-                @else
-                <a class="btn btn-primary btn-lg btn-warning text-center startbtn" href="{{ route('home') }}">
-                    Go to My Account
-                </a>
+                    {{ $success }}
+                </div>
                 @endif
-          </div>
-        </div>
-      </div>
-    </section> -->
-    <!-- End Promo Block -->
 
-    <!-- Icon Blocks -->
-    <section class="g-py-100">
-      <div class="container">
-        <div class="row no-gutters">
-          <div class="col-lg-4 g-px-40 g-mb-50 g-mb-0--lg">
-            <!-- Icon Blocks -->
-            <div class="text-center">
-              <span class="solid-bg-icon d-inline-block u-icon-v3 u-icon-size--xl g-bg-primary g-color-white rounded-circle g-mb-30">
-                <i class="fal fa-dollar-sign"></i>
-              </span>
-              <h3 class="h5 g-color-gray-dark-v2 g-font-weight-600 text-uppercase mb-3">7 Day Free Trial</h3>
-              <p class="mb-0">No risk 7 day free trial. Just pay $1 for your 3 bureau credit report.</p>
+                @if (isset($errors) && !empty($errors))
+                @foreach ($errors as $error)
+                <div class="col-md-8 col-md-offset-2 alert alert-danger" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    {{ $error }}
+                </div>
+                @endforeach
+                @endif
+
             </div>
-            <!-- End Icon Blocks -->
-          </div>
-
-          <div class="col-lg-4 g-brd-left--lg g-brd-gray-light-v4 g-px-40 g-mb-50 g-mb-0--lg">
-            <!-- Icon Blocks -->
-            <div class="text-center">
-              <span class="solid-bg-icon d-inline-block u-icon-v3 u-icon-size--xl g-bg-primary g-color-white rounded-circle g-mb-30">
-                <i class="fal fa-shield-check"></i>
-              </span>
-              <h3 class="h5 g-color-gray-dark-v2 g-font-weight-600 text-uppercase mb-3">Fraud &amp; Credit Monitoring</h3>
-              <p class="mb-0">Get daily credit, fraud and identity theft monitoring. $1M dollar ID theft policy included.</p>
-            </div>
-            <!-- End Icon Blocks -->
-          </div>
-
-          <div class="col-lg-4 g-brd-left--lg g-brd-gray-light-v4 g-px-40">
-            <!-- Icon Blocks -->
-            <div class="text-center">
-              <span class="solid-bg-icon d-inline-block u-icon-v3 u-icon-size--xl g-bg-primary g-color-white rounded-circle g-mb-30">
-                <i class="fal fa-lock-alt"></i>
-              </span>
-              <h3 class="h5 g-color-gray-dark-v2 g-font-weight-600 text-uppercase mb-3">Secure Online Delivery</h3>
-              <p class="mb-0">Get instant unlimited access to your credit reports via your secure account.</p>
-            </div>
-            <!-- End Icon Blocks -->
-          </div>
         </div>
-      </div>
-    </section>
-    <!-- End Icon Blocks -->
 
-    <hr class="g-brd-gray-light-v4 my-0">
-
-    <!-- About Our Company -->
-    <section class="1g-bg-secondary g-py-100">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6 align-self-center g-mb-100 g-mb-0--lg">
-            <header class="u-heading-v2-3--bottom g-brd-primary g-mb-20">
-              <h2 class="h4 u-heading-v2__title g-color-gray-dark-v2 g-font-weight-600 text-uppercase">About See Your Score</h2>
-            </header>
-
-            <p class="g-font-size-16 g-mb-30">See Your Score was founded to provide easy access for everyone to their credit scores with a full credit report from all three bureaus.</p>
-
-            <ul class="list-unstyled g-color-gray-dark-v4 mb-0">
-              <li class="d-flex g-mb-10">
-                <i class="icon-check g-color-primary g-mt-5 g-mr-10"></i> Daily Credit Monitoring &amp; Alerts
-              </li>
-              <li class="d-flex g-mb-10">
-                <i class="icon-check g-color-primary g-mt-5 g-mr-10"></i> $1M ID Theft Policy
-              </li>
-              <li class="d-flex g-mb-10">
-                <i class="icon-check g-color-primary g-mt-5 g-mr-10"></i> Fraud &amp; Identity Theft Monitoring
-              </li>
-              <li class="d-flex g-mb-10">
-                <i class="icon-check g-color-primary g-mt-5 g-mr-10"></i> Toll Free Customer Service
-              </li>
-            </ul>
-          </div>
-
-          <div class="col-lg-6 g-bg-img-hero g-pos-rel" style="background-image: url('{{ asset('i/text-bg.png') }}');">
-            <div class="g-absolute-centered text-center w-100 g-px-40">
-              <h2 class="h1 g-font-weight-600 g-letter-spacing-0_5">
-                  <span class="u-text-animation u-text-animation--typing"></span></h2>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- End About Our Company -->
-
-
-    <!-- Most Quality Solution -->
-    <section class="dzsparallaxer auto-init height-is-based-on-content use-loading" data-options='{direction: "reverse", settings_mode_oneelement_max_offset: "150"}'>
-      <!-- Parallax Image -->
-      <div class="divimage dzsparallaxer--target w-100 u-bg-overlay g-bg-img-hero g-bg-white-opacity-0_7--after" style="height: 140%; background-image: url('{{ asset('i/banner3.jpg') }}');"></div>
-      <!-- End Parallax Image -->
-
-      <div class="container u-bg-overlay__inner g-py-150--md g-py-80">
-        <div class="row">
-          <!-- Section Content -->
-          <div class="col-lg-6 order-2 order-sm-1 g-mb-0 g-mb-50--sm g-mb-0--lg">
-            <div class="u-heading-v2-3--bottom g-brd-primary g-mb-30">
-              <h2 class="h4 u-heading-v2__title g-color-gray-dark-v2 g-font-weight-600 text-uppercase">Your Latest Credit Score</h2>
-            </div>
-            <p class="g-font-size-16 g-line-height-2 g-mb-30">Our goal is helping you make wise credit decisions by giving you quick and simple access to your credit reports and scores. Online signup in just minutes, with a FREE 7 day trial and easy online cancellation.</p>
-          </div>
-          <!-- End Section Content -->
-        </div>
-      </div>
-    </section>
-    <!-- End Most Quality Solution -->
-
-
-
-    <!-- Call To Action -->
-    <section class="g-bg-primary g-color-white g-pa-30">
-      <div class="d-md-flex justify-content-md-center text-center">
-        <div class="align-self-md-center">
-          <p class="lead g-font-weight-400 g-mr-20--md g-mb-15 g-mb-0--md">Get your 3 bureau credit report now for just $1</p>
-        </div>
-        <div class="align-self-md-center">
-          <a class="btn btn-lg u-btn-white text-uppercase g-font-weight-600 g-font-size-12" href="/register">Begin Registration</a>
-        </div>
-      </div>
-    </section>
-    <!-- End Call To Action -->
+        @yield('content')
+    </div>
 
     <!-- Footer -->
     <div id="contacts-section" class="g-bg-black-opacity-0_9 g-color-white-opacity-0_8 g-py-60">
