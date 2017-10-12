d921e2f1a72153d3ae43d1735caca7efff49a5ff
Lance Sorenson <lancesorenson@gmail.com>
2017-10-12 01:28:55 -0600

    Redirect admins to Admin Dashboard instead of general user home

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,21 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Where to redirect users after login. Handles user permissions and redirects accordingly
+     *
+     * @return string Path the user should be redirected
+     */
+    protected function redirectTo()
+    {
+        switch (Auth::user()->role) {
+            case "superadmin":
+            case "admin":
+                return '/admin';
+            default:
+                return '/home';
+        }
     }
 }
