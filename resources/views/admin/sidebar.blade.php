736ab9b572cc23c404c1c5f4355605ac9a5a5552
Lance Sorenson <lancesorenson@gmail.com>
2017-10-11 22:37:40 -0600

    Updated icons on sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="list-group">
     <a class="list-group-item" href="{{ url('admin') }}"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp; Dashboard</a>
     <a class="list-group-item" href="{{ url('admin/users') }}"><i class="fa fa-user fa-fw" aria-hidden="true"></i>&nbsp; Users</a>
-    <a class="list-group-item" href="{{ url('admin/charges') }}"><i class="fa fa-pencil fa-fw" aria-hidden="true"></i>&nbsp; Charges</a>
-    <a class="list-group-item" href="{{ url('admin/subscriptions') }}"><i class="fa fa-cog fa-fw" aria-hidden="true"></i>&nbsp; Subscriptions</a>
+    <a class="list-group-item" href="{{ url('admin/charges') }}"><i class="fa fa-credit-card fa-fw" aria-hidden="true"></i>&nbsp; Charges</a>
+    <a class="list-group-item" href="{{ url('admin/subscriptions') }}"><i class="fa fa-usd fa-fw" aria-hidden="true"></i>&nbsp; Subscriptions</a>
 </div>
 
