e84c60ae34a18ad77d6f01b18fab427f08483625
Lance Sorenson <lancesorenson@gmail.com>
2017-10-12 04:50:56 -0600

    Rearranging Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,16 +3,34 @@
 @section('content')
 <div class="container">
     <div class="row">
-        @include('admin.sidebar')
+        <div class="col-md-3">
+            @include('admin.sidebar')
+        </div>
 
         <div class="col-md-9">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+            <div class="panel"></div>
 
+            <h3>Recently Registered</h3>
+            <div class="panel panel-default">
                 <div class="panel-body">
-                    Your application's dashboard.
+                    Last 5-10 people registered
                 </div>
             </div>
+
+            <h3>Subscriptions</h3>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    Last 5-10 subscribers
+                </div>
+            </div>
+
+            <h3>Stats</h3>
+            <div class="panel panel-default">
+                <div class="panel-body">
+
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,7 +1,10 @@
-<div class="list-group">
-    <a class="list-group-item" href="{{ url('admin') }}"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp; Dashboard</a>
-    <a class="list-group-item" href="{{ url('admin/users') }}"><i class="fa fa-user fa-fw" aria-hidden="true"></i>&nbsp; Users</a>
-    <a class="list-group-item" href="{{ url('admin/charges') }}"><i class="fa fa-credit-card fa-fw" aria-hidden="true"></i>&nbsp; Charges</a>
-    <a class="list-group-item" href="{{ url('admin/subscriptions') }}"><i class="fa fa-usd fa-fw" aria-hidden="true"></i>&nbsp; Subscriptions</a>
+<div class="list-group" style="margin-top: 70px;">
+    <a class="list-group-item {{ Request::is('admin') ? ' active' : '' }}" href="{{ url('admin') }}">
+        <i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp; Dashboard
+    </a>
+    <a class="list-group-item {{ Request::is('admin/users') ? ' active' : '' }}" href="{{ url('admin/users') }}">
+        <i class="fa fa-user fa-fw" aria-hidden="true"></i>&nbsp; Users
+    </a>
+
 </div>
 
